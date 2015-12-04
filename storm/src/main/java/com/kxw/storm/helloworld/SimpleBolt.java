cb8c5ead3f428135c20bd848dc533db3d3d749c5
kingson.wu <kingson.wu@vipshop.com>
2015-12-04 14:51:04 +0800

    storm

---
+++ storm/src/main/java/com/kxw/storm/helloworld/SimpleBolt.java
@@ -0,0 +1,41 @@
+package com.kxw.storm.helloworld;
+
+import backtype.storm.topology.BasicOutputCollector;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+/**
+ * {<a href='http://www.cnblogs.com/zhangyukun/p/4031066.html'>@link</a>}
+ */
+
+public class SimpleBolt extends BaseBasicBolt {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public void execute(Tuple input,BasicOutputCollector collector) {
+        try {
+            String msg = input.getString(0);
+            if (msg != null){
+                //System.out.println("msg="+msg);
+                collector.emit(new Values(msg + "msg is processed!"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void declareOutputFields(
+            OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("info"));
+
+    }
+
+}
